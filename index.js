df492beedef4aa31b7275dca4313c40de9942009
Tan Tano <tantn1@sunshinegroup.vn>
2020-06-20 10:07:32 +0700

    Xu li reponsive slide mon an

---
+++ index.js
@@ -0,0 +1,23 @@
+$(function() {
+    setHeight();
+    $(window).resize(function() {
+        setHeight();
+    });
+});
+
+function setHeight() {
+    var soluongmon = $('.motmon').length;
+    var docao = soluongmon * $('.motmon').height();
+    let isMobile = window.matchMedia("(max-width: 576px)").matches;
+    let isTablet = window.matchMedia("(max-width: 768px)").matches;
+    if (isMobile && isTablet) {
+        // Trường hợp mobile
+        $('.thucdonct .noidungct').height(docao);
+    } else if (!isMobile && isTablet) {
+        // Trường hợp tablet
+        $('.thucdonct .noidungct').height(docao/2);
+    } else {
+        // Trường hợp thường
+        $('.thucdonct .noidungct').height(docao/3);
+    }
+}
